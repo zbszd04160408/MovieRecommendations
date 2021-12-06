83de27e21fbfb4f7fb25cd4b353e4b8459d5148e
Weiqing Huang <viavivian0.0@gmail.com>
2021-12-06 16:38:03 -0600

    update

--- 60-dashboard.py
+++ 60-dashboard.py
@@ -51,7 +51,7 @@ functions = ['Please Select', 'Ratings Recommendation', 'Content Recommendations
 option = st.sidebar.selectbox('Please select the function you would like to use:', functions)
 
 main_df_filepath = Path(__file__).parents[1] / 'data/IMDb_movies.csv'
-
+st.write(Path(__file__).parents)
 st.write(main_df_filepath)
 print(main_df_filepath)
 
